0f7b469078627969fb7079f3aea3ab1c110c8f44
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2023-11-30 12:39:03 +0200

    Change ios deployment version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "zenix-common",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15), .iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
